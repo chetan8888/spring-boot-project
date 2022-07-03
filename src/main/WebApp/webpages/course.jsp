3d69badad300f898d2fa34bad2af58a0a6f78745
CHETAN GOUR <chetangour88@gmail.com>
2022-07-02 19:43:22 -0700

    passing parameters from client to server

--- src/main/WebApp/webpages/course.jsp
+++ src/main/WebApp/webpages/course.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Course
+	Course: ${cname}
 </body>
 </html>
--- src/main/java/com/example/demo/CoursesController.java
+++ src/main/java/com/example/demo/CoursesController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CoursesController {
 	
 	@RequestMapping("/course")
-	public String courses() {
+	public String courses(HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		
+		String cname = req.getParameter("cname");
+		session.setAttribute("cname", cname);
 		return "course";
 	}
 

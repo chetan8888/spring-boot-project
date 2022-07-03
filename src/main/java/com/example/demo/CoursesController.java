cefc90a1d8bbc40234957a32f299229ba602b40a
CHETAN GOUR <chetangour88@gmail.com>
2022-07-02 20:19:17 -0700

    Getting url params via ModelAndView

--- src/main/java/com/example/demo/CoursesController.java
+++ src/main/java/com/example/demo/CoursesController.java
@@ -1,22 +1,21 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CoursesController {
 	
 	@RequestMapping("/course")
-	public String courses(HttpServletRequest req) {
+	public ModelAndView courses(@RequestParam("cname") String courseName) {
 		
-		HttpSession session = req.getSession();
-		
-		String cname = req.getParameter("cname");
-		session.setAttribute("cname", cname);
-		return "course";
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("course");
+		mv.addObject("cname", courseName);
+		return mv;
+				
 	}
 
 }
